89cce70d33193d40354e520f66355b8d45b6d517
skullcrushr11 <118558979+skullcrushr11@users.noreply.github.com>
2025-06-24 17:14:25 +0530

    Update train.py init tokenizer

--- bunny/train/train.py
+++ bunny/train/train.py
@@ -227,6 +227,13 @@ def train():
             trust_remote_code=True
         )
     if model_args.model_type == 'llama3-8b':
+            tokenizer = transformers.AutoTokenizer.from_pretrained(
+            model_args.model_name_or_path,
+            cache_dir=training_args.cache_dir,
+            model_max_length=training_args.model_max_length,
+            padding_side="right",
+            use_fast=True,
+        )
         tokenizer.eos_token_id = 128001
         tokenizer.pad_token = tokenizer.eos_token
 
